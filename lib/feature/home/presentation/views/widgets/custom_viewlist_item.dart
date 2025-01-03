ed9926acffb8c6476889f604d298a15744ccf2ed
Shaban <sm175075@gmail.com>
2025-01-03 18:20:23 +0200

    add font family

--- lib/feature/home/presentation/views/widgets/custom_viewlist_item.dart
+++ lib/feature/home/presentation/views/widgets/custom_viewlist_item.dart
@@ -11,11 +11,14 @@ class CustomListViewItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return AspectRatio(
       aspectRatio: 2.5/4,
-      child: Container(
-        decoration: const BoxDecoration(
-          color: Colors.red,
-          borderRadius: BorderRadius.all(Radius.circular(10),),
-         image:DecorationImage(image: AssetImage(AssetsApp.testImage),fit: BoxFit.fill),
+      child: Padding(
+        padding: const EdgeInsets.only(right: 12.0),
+        child: Container(
+          decoration: const BoxDecoration(
+            color: Colors.red,
+            borderRadius: BorderRadius.all(Radius.circular(10),),
+           image:DecorationImage(image: AssetImage(AssetsApp.testImage),fit: BoxFit.fill),
+          ),
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:bookly_app/conestant.dart';
 import 'package:bookly_app/feature/splash/presentation/splash_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 
 void main(){
@@ -17,6 +18,7 @@ class BooklyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: kPrimaryColor,
+        textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
       ),
       home:const SplashView(),
     );
