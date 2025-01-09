4f0a7f6e51ff5062128e62f152e4ca1d071938f7
Shaban <sm175075@gmail.com>
2025-01-09 20:36:27 +0200

    add rating to book details view

--- lib/feature/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/feature/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:bookly_app/core/utils/styles.dart';
+import 'package:bookly_app/feature/home/presentation/views/widgets/book_rating.dart';
 import 'package:bookly_app/feature/home/presentation/views/widgets/custom_book_details_appbar.dart';
 import 'package:bookly_app/feature/home/presentation/views/widgets/custom_book_image.dart';
 import 'package:flutter/material.dart';
@@ -31,8 +32,10 @@ class BookDetailsViewBody extends StatelessWidget {
               style:Styles.textStyle18.copyWith(fontStyle: FontStyle.italic,fontWeight: FontWeight.w500,),
                        ),
            ),
+           const SizedBox(height: 16,),
+           const BookRating(mainAxisAlignment: MainAxisAlignment.center,)
         ],
-        
+
       ),
     );
   }
--- lib/feature/home/presentation/views/widgets/book_rating.dart
+++ lib/feature/home/presentation/views/widgets/book_rating.dart
@@ -5,19 +5,24 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class BookRating extends StatelessWidget {
   const BookRating({
-    super.key,
+    super.key,  this.mainAxisAlignment=MainAxisAlignment.start,
   });
-
+ final MainAxisAlignment mainAxisAlignment;
   @override
   Widget build(BuildContext context) {
     return 
         Row( 
+          mainAxisAlignment: mainAxisAlignment,
          children: [
-           const Icon(FontAwesomeIcons.solidStar,color:AppColors.kYellowColor,),
+           const Icon(
+            size: 14,
+            FontAwesomeIcons.solidStar,color:AppColors.kYellowColor,),
            const SizedBox(width: 6.3,),
            const Text('4.8',style: Styles.textStyle16,),
            const SizedBox(width: 5,),
-           Text('(2360)',style: Styles.textStyle14.copyWith(color: AppColors.kGreyColor),),
+           Opacity(
+            opacity: 0.5,
+            child: Text('(2360)',style: Styles.textStyle14.copyWith(fontWeight: FontWeight.w600),)),
          ],
     );
   }
